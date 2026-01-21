e9d9b220123c3b2c4e75ee45c76938082a02646f
Rich <rich@datarich.org>
2026-01-20 22:13:58 -0500

    update resume content

--- brew-resume-install.rb
+++ brew-resume-install.rb
@@ -2,7 +2,7 @@ class RichGoldstein < Formula
   desc "Man-page resume for Rich Goldstein"
   homepage "https://github.com/au79stein/unix-resume"
   url "https://github.com/au79stein/unix-resume/archive/v1.0.0.tar.gz"
-  sha256 "SHA256_HERE"
+  sha256 "270404eed5fcf0d8e46db3fbafc778c69d0248944073cf6ee3f725636793e1b1"
 
   def install
     man1.install "resume.roff" => "rich-goldstein.1"
